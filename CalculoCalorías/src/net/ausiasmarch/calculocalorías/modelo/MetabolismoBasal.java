1e97f01d7159c557a504862a565c82404457764a
PixelZer0 <pixel_zero@hotmail.com>
2016-10-27 10:53:25 +0200

    Añadida estructura visual de la aplicación

---
+++ CalculoCalorías/src/net/ausiasmarch/calculocalorías/modelo/MetabolismoBasal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.ausiasmarch.calculocalorías.modelo;
+
+/**
+ *
+ * @author a021704062c
+ */
+public class MetabolismoBasal {
+    
+}
